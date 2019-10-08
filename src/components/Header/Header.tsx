22b17c00efc729cbe47d2387c0951bdaf99ad8c9
Aldeon Arka <aldeonmoriak@gmail.com>
2019-10-08 16:51:30 +0330

    added fonts and the header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,2 +1,73 @@
+import AppBar from "@material-ui/core/AppBar";
+import { purple } from "@material-ui/core/colors";
+import {
+  createMuiTheme,
+  createStyles,
+  makeStyles,
+  Theme
+} from "@material-ui/core/styles";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import { ThemeProvider } from "@material-ui/styles";
 import React from "react";
-import {} from "@material-ui/core";
+import nativeClasses from "./Header.module.css";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1
+    },
+    menuButton: {
+      marginRight: theme.spacing(2)
+    },
+    title: {
+      flexGrow: 1
+    }
+  })
+);
+
+function ButtonAppBar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root + ", " + nativeClasses.Header}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h5" className={classes.title}>
+            برنامه بخور نخور خونه ما
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: [
+      "B Koodak",
+      "Roboto",
+      '"Segoe UI"',
+      '"Helvetica Neue"',
+      "Arial",
+      "sans-serif",
+      '"Apple Color Emoji"',
+      '"Segoe UI Emoji"',
+      '"Segoe UI Symbol"'
+    ].join(",")
+  },
+  palette: {
+    primary: {
+      main: "#607d8b"
+    },
+    secondary: purple
+  }
+});
+
+export default function FontAssignHandler() {
+  return (
+    <ThemeProvider theme={theme}>
+      <ButtonAppBar />
+    </ThemeProvider>
+  );
+}
---
+++ src/containers/Form/ExpenseForm/ExpenseForm.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Form: React.FC = () => {
+  return (
+    <div>
+      <p>ممد همه ش رو خورده</p>
+    </div>
+  );
+};
+
+export default Form;
